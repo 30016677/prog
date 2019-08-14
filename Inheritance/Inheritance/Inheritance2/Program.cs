8f1735ff57515cecd8e46a96d0ef7bd82feaafa2
Kris Boyte <30016677@student.toiohomai.ac.nz>
2019-08-14 15:57:28 +1200

    14/8/19

--- Inheritance/Inheritance/Inheritance2/Program.cs
+++ Inheritance/Inheritance/Inheritance2/Program.cs
@@ -10,18 +10,59 @@ namespace Inheritance2
     {
         static void Main(string[] args)
         {
+            Animal animal01 = new Animal();
 
+            bool b = true;
+
+            
+            do
+            {
+                Console.WriteLine("Would you like to view animals or add a new animal? (view/add)");
+                string input = Console.ReadLine();
+                if (input == "view")
+                {
+                    Console.WriteLine(animal01.GetAnimal());
+                }
+                else if (input == "add")
+                {
+                    
+                    Console.WriteLine("Type?");
+                    animal01.Type = Console.ReadLine();
+                    Console.WriteLine("Name?");
+                    animal01.Name = Console.ReadLine();
+                    Console.WriteLine("ID?");
+                    animal01.Id = Console.ReadLine();
+                }
+            } while (b);
+
+            
+            
+
+
+            Console.ReadLine();
         }
     }
     class Animal
     {
+        public string Id { get; set; }
+        public string Name { get; set; }
         public string Type { get; set; }
-        
+
+        public Animal()
+        {
+        }
+
+        public string GetAnimal()
+        {
+            return Id + Name + Type;
+        }
     }
     class Pig : Animal
     {
+        public List<string> Count { get; set; }
 
-    }
+
+    }   
     class Sheep : Animal
     {
 
